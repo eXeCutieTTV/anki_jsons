8a6a335532fa55505c753abdfabae9f5f133214b
eXeCutie <danielgehlert2007@gmailcom>
2026-02-23 16:53:54 +0100

    syntax

---
+++ data/japanese/katakana.js
@@ -0,0 +1,33 @@
+let temp = [];
+function katakana() {
+    const trs = document.querySelector(".wikitable.nowraplinks").querySelectorAll('tr');
+
+    for (let i = 2; i < 12; i++) {
+        //console.log(trs[i])
+        const tds = trs[i].querySelectorAll('td');
+        for (let j = 0; j < 5; j++) {
+            //console.log(tds[j])
+            const result = {
+                Kana: tds[j].querySelector("a").innerText.replace(/[\[\]]/g, "").trim(),
+                Reading: tds[j].querySelector("a").title.replace("(kana)", "").trim(),
+                Strokes: ""
+            }
+            temp.push(result);
+        }
+    }
+    for (let i = 15; i < 19; i++) {
+        //console.log(trs[i])
+        const tds = trs[i].querySelectorAll('td');
+        for (let j = 0; j < 5; j++) {
+            //console.log(tds[j])
+            const result = {
+                Kana: tds[j].querySelectorAll("span")[1].innerText.replace(/[\[\]]/g, "").trim(),
+                Reading: tds[j].querySelectorAll("span")[2].innerText.trim(),
+                Strokes: ""
+            }
+            temp.push(result);
+        }
+    }
+}
+katakana();
+console.log(temp);
